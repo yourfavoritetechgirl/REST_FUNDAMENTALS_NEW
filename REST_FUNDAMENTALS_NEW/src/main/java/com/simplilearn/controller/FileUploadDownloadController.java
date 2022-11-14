a13ccd4e7d08e174a83a88f0a43bd113fd611eb8
Savyata Gomden <savyatag@amdocs.com>
2022-11-14 16:20:34 -0600

    File Upload & Download

--- REST_FUNDAMENTALS_NEW/src/main/java/com/simplilearn/controller/FileUploadDownloadController.java
+++ REST_FUNDAMENTALS_NEW/src/main/java/com/simplilearn/controller/FileUploadDownloadController.java
@@ -1,10 +1,15 @@
 package com.simplilearn.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,6 +35,21 @@ public class FileUploadDownloadController {
 		fos.close();
 		
 		return "File uploaded successfully";
+	}
+	
+	@RequestMapping(value="/download", method=RequestMethod.GET)
+	public ResponseEntity<Object>downloadFile(@RequestParam("fileName") String fileName) throws FileNotFoundException{
+		
+		//Write code to read file
+		File downloadFie = new File("c:/temp/"+fileName);
+		InputStreamResource resource = new InputStreamResource(new FileInputStream(downloadFie));
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Disposition", "attachment; fileName="+fileName);
+		
+		return ResponseEntity.ok().headers(headers).contentType(MediaType.parseMediaType("application/txt")).
+		body(resource);
+		
 	}
 
 }
